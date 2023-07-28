eb2d9381e1e4f0a72bfccbeafbfe2c22b4a5cd0a
Krish1971997 <krishnakumar1971997@gmail.com>
2023-07-28 08:20:13 +0530

    changed from intelliji

--- src/busBookingSystem/BookingHistory.java
+++ src/busBookingSystem/BookingHistory.java
@@ -10,6 +10,7 @@ public class BookingHistory {
 	static Map<Integer, Customers> CancelTickets = new LinkedHashMap<>();
 	static int cancelTicketsSeries = 1;
 
+	//Changed from Intelliji
 	public static void bookingHistory(Map<String, Customers> history) {
 		for (Entry<String, Customers> map : history.entrySet()) {
 			if (map.getValue() != null) {
